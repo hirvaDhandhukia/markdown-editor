8ce38c78851259176f8eef3aa3dd692d1fe168b4
Hirva Dhandhukia <hsdhandhukia@gmail.com>
2022-01-25 03:00:06 +0530

    add: working (ready to use)

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
+import React, {useState} from 'react';
+import ReactMarkdown from 'react-markdown';
 import './App.css';
 
 function App() {
+  const [markdown, setMarkdown] = useState('# markdown preview')
+
   return (
     <div className="App">
-      hi!
+      {/* setting up a section (main div) */}
+      <section className='markdown'>
+        {/* input will have a value of 'markdown' to initialize it with some dummy-preview text */}
+        <textarea
+          className='input'
+          value={markdown}
+          onChange={(e) => setMarkdown(e.target.value)}
+        ></textarea>
+        <article className='result'>
+          {/* using default markdown view by passing jsx markdown input */}
+          <ReactMarkdown>{markdown}</ReactMarkdown>
+        </article>
+      </section>
     </div>
   );
 }
